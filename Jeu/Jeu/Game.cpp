26a0db5ca97061996b1bf9b0b43d9b7b10966e21
Renaud Belloncle <renaud.belloncle@orange.fr>
2015-12-06 18:15:13 +0100

    Gestion de la taille des sprites dans le déplacement et mise en place de limites pour le zoom

--- Jeu/Jeu/Game.cpp
+++ Jeu/Jeu/Game.cpp
@@ -13,6 +13,7 @@ Game::Game()
     c_view[0] = WIN_WIDTH/2;
     c_view[1] = WIN_HEIGTH/2;
     m_view = sf::View(sf::Vector2f(c_view[0],c_view[1]),sf::Vector2f(WIN_WIDTH,WIN_HEIGTH));
+    m_view.zoom(2);
     m_window.setFramerateLimit(60);
 }
 
